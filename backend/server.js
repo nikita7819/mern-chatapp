c7ca5bba05ead1b8a13cd1c107924bd02a587f28
Nikita <nikitakubavat33@gmail.com>
2026-05-05 12:31:44 +0530

    Cors settings updated

--- backend/server.js
+++ backend/server.js
@@ -67,7 +67,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    // origin: "https://chat-client-mzlo.onrender.com",
+    origin: "https://chat-client-mzlo.onrender.com",
   },
 });
 
